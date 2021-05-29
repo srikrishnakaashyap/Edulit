95548e2ed3c7ea6c79b46db19a1232c26add4b1b
Kaashyap <kaashyap@acceleron.dev>
2021-05-29 20:12:30 +0530

    fix html and make room.html

--- app/api/join_room.py
+++ app/api/join_room.py
@@ -10,17 +10,11 @@ room_blueprint = Blueprint('room', __name__, template_folder="templates")
 def join_room():
   room_id = request.args.get('room_id')
   username = current_user.first_name
-
   room = Room.query.get(room_id)
-
   created_by = room.user.first_name
 
-
-
-
-
   if room_id:
-    return render_template('DUMMY.html', room_id=room_id, username=username, room_name=room.name, created_by=created_by)
+    return render_template('room.html', room_id=room_id, username=username, room_name=room.name, created_by=created_by)
   else:
     return redirect(url_for('home.home'))
 
